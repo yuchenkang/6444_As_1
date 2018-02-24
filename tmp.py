99faa9a8af7835f1953205bb8f3d9a5aa31e26d3
yuchenkang <kangyuchen123@gmail.com>
2018-02-24 13:01:39 -0500

    Add files via upload

--- tmp.py
+++ tmp.py
@@ -8,29 +8,32 @@ import os
 import re
 
 count=0
-c=0
-root= ".\\eclipse_jdt"
-wn = ".\\xxx.txt"
+
+r= "./Workable"
+wn = "./zzz.txt"
 w=open(wn,'w')
 
 print("read every file")
-for root, dirs, files in os.walk(root):
+for root, dirs, files in os.walk(r):
+    s=set()
     for filename in files:
-        c=c+1
+        
         f = open(os.path.join(root, filename), "r")
         #print(os.path.join(root, filename))
         count=count+1
-        if count%20000==0:
+        if count%3000==0:
             print(count)
         content = f.read().lower()
         match = re.search("(bug|bugs|fix|fixed)[# \t]*([0-9]+)", content)
         if match:
-            w.write(match.group(2))
-            print(os.path.join(root, filename))
-            w.write(root)
-            w.write("\n")
+            s.add(match.group(2))
         f.close
-print(str(c)+"\n")        
+    for bug in s:
+        w.write(bug)
+        w.write("\t")
+        w.write(str(root)[len(r)+1:])
+        w.write("\n")
+      
 print(str(count)+"\n")        
 w.seek(0)
 w.close
